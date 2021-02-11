a686583f95f2f4e956e12a06e0458768e83d3403
Farhan <farhanhm12@gmail.com>
2021-02-10 22:03:05 -0500

    added images

--- src/pages/Plant.js
+++ src/pages/Plant.js
@@ -11,9 +11,9 @@ export default class Plants extends Component {
                     <div>plant slider man</div>
                     <div className="plant-list">
                         <PlantSlider plants={BellPepperImages} />
-                        <PlantSlider plants={BellPepperImages} />
-                        <PlantSlider plants={BellPepperImages} />
-                        <PlantSlider plants={BellPepperImages} />
+                        <PlantSlider plants={TomatoMainImages} />
+                        <PlantSlider plants={TomatoSeedImages} />
+                        <PlantSlider plants={PepperSeedImages} />
                     </div>
                 </div>
             </section>
@@ -31,4 +31,40 @@ const BellPepperImages = [
     { descp: 'logo 8', img: require('../media/plants/2020/bellpeppers/08.jpg') },
     { descp: 'logo 9', img: require('../media/plants/2020/bellpeppers/09.jpg') },
     { descp: 'logo 10', img: require('../media/plants/2020/bellpeppers/10.jpg') },
+];
+const TomatoMainImages = [
+    { descp: 'logo 1', img: require('../media/plants/2020/tomatomain/01.jpg') },
+    { descp: 'logo 2', img: require('../media/plants/2020/tomatomain/02.jpg') },
+    { descp: 'logo 3', img: require('../media/plants/2020/tomatomain/03.jpg') },
+    { descp: 'logo 4', img: require('../media/plants/2020/tomatomain/04.jpg') },
+    { descp: 'logo 5', img: require('../media/plants/2020/tomatomain/05.jpg') },
+    { descp: 'logo 6', img: require('../media/plants/2020/tomatomain/06.jpg') },
+    { descp: 'logo 7', img: require('../media/plants/2020/tomatomain/07.jpg') },
+    { descp: 'logo 8', img: require('../media/plants/2020/tomatomain/08.jpg') },
+    { descp: 'logo 9', img: require('../media/plants/2020/tomatomain/09.jpg') },
+];
+
+const TomatoSeedImages = [
+    { descp: 'logo 1', img: require('../media/plants/2020/tomatoseeds/01.jpg') },
+    { descp: 'logo 2', img: require('../media/plants/2020/tomatoseeds/02.jpg') },
+    { descp: 'logo 3', img: require('../media/plants/2020/tomatoseeds/03.jpg') },
+    { descp: 'logo 4', img: require('../media/plants/2020/tomatoseeds/04.jpg') },
+    { descp: 'logo 5', img: require('../media/plants/2020/tomatoseeds/05.jpg') },
+    { descp: 'logo 6', img: require('../media/plants/2020/tomatoseeds/06.jpg') },
+    { descp: 'logo 7', img: require('../media/plants/2020/tomatoseeds/07.jpg') },
+    { descp: 'logo 9', img: require('../media/plants/2020/tomatoseeds/09.jpg') },
+    { descp: 'logo 9', img: require('../media/plants/2020/tomatoseeds/10.jpg') },
+    { descp: 'logo 9', img: require('../media/plants/2020/tomatoseeds/11.jpg') },
+];
+
+const PepperSeedImages = [
+    { descp: 'logo 1', img: require('../media/plants/2020/Pepper/01.jpg') },
+    { descp: 'logo 2', img: require('../media/plants/2020/Pepper/02.jpg') },
+    { descp: 'logo 3', img: require('../media/plants/2020/Pepper/03.jpg') },
+    { descp: 'logo 4', img: require('../media/plants/2020/Pepper/04.jpg') },
+    { descp: 'logo 5', img: require('../media/plants/2020/Pepper/05.jpg') },
+    { descp: 'logo 6', img: require('../media/plants/2020/Pepper/06.jpg') },
+    { descp: 'logo 7', img: require('../media/plants/2020/Pepper/07.jpg') },
+    { descp: 'logo 9', img: require('../media/plants/2020/Pepper/09.jpg') },
+    { descp: 'logo 9', img: require('../media/plants/2020/Pepper/10.jpg') },
 ];
